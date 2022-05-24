32f996db8c671e86244cd293a08e44141e1b6a11
Pedro <paraoz22@gmail.com>
2022-05-24 14:42:42 -0300

    added google idToken verification

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,7 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.springframework.boot:spring-boot-starter-security:2.7.0")
+	implementation("com.google.api-client:google-api-client:1.32.1")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
--- src/main/kotlin/com/example/demo/service/AuthenticationService.kt
+++ src/main/kotlin/com/example/demo/service/AuthenticationService.kt
@@ -1,15 +1,22 @@
 package com.example.demo.service
 
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier
+import com.google.api.client.http.javanet.NetHttpTransport
+import com.google.api.client.json.gson.GsonFactory
 import org.springframework.stereotype.Service
 
 @Service
 class AuthenticationService {
 
-  private val clientId = ""
+  private val clientIds = listOf("64992691436-bfdvk9u682iut84mk1f9kvbll44u5dqt.apps.googleusercontent.com")
 
-  private val verifier = clientId
+  private val verifier = GoogleIdTokenVerifier.Builder(
+    NetHttpTransport(), GsonFactory.getDefaultInstance()
+  ).setAudience(clientIds).build()
 
   fun authenticate(idToken: String): String? {
-    return "test-email@mail.com"
+    val googleIdToken: GoogleIdToken? = verifier.verify(idToken)
+    return googleIdToken?.payload?.email
   }
 }
